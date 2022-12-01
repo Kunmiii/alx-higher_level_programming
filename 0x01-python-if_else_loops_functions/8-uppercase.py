9e0b31a99e84ed50260ed6dd7fa04578192cf397
Oluwatimileyin <oke_joast@protonmail.com>
2022-11-30 17:47:53 -0800

    To Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
+    tmp = list(str)
     for i in range(1):
         if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
             tmp[i] = chr(ord(tmp[i]) - 32)
